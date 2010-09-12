a60a0d693d394f51ad33dfcb5deec80a4a6d315b
Hugo Duncan <hugo_duncan@yahoo.com>
2010-09-11 23:55:10 -0400

    Added haproxy crates and nodes

--- webapp-nodes/src/webapp_nodes/crates.clj
+++ webapp-nodes/src/webapp_nodes/crates.clj
@@ -3,6 +3,7 @@
   (:require
    [pallet.resource :as resource]
    [pallet.crate.automated-admin-user :as automated-admin-user]
+   [pallet.crate.haproxy :as haproxy]
    [pallet.crate.java :as java]
    [pallet.crate.tomcat :as tomcat]))
 
@@ -12,12 +13,30 @@
   (automated-admin-user/automated-admin-user request))
 
 (defn tomcat
+  "Tomcat server configuration"
   [request]
   (-> request
       (java/java :openjdk)
       (tomcat/tomcat)))
 
 (defn tomcat-deploy
+  "Tomcat deploy as ROOT application"
   [request path]
   (-> request
       (tomcat/deploy "ROOT" :local-file path :clear-existing true)))
+
+(defn haproxy
+  "haproxy server with app1 on port 80."
+  [request]
+  (-> request
+      (haproxy/install-package)
+      (haproxy/configure
+       :listen {:app1 {:server-address "0.0.0.0:80"
+                       :balance "roundrobin"}})))
+
+(defn reverse-proxy
+  "Declare node as being reverse proxied by haproxy on tag."
+  [request tag app port]
+  (-> request
+      (haproxy/proxied-by
+       tag app :server-port port :check true :weight 1 :maxconn 50)))
--- webapp-nodes/src/webapp_nodes/nodes.clj
+++ webapp-nodes/src/webapp_nodes/nodes.clj
@@ -5,12 +5,38 @@
    [pallet.resource.service :as service]
    [webapp-nodes.crates :as crates]))
 
-(core/defnode
-  webapp []
+(core/defnode webapp
+  "Basic web app, serverd by tomcat"
+  []
   :bootstrap (resource/phase
               (crates/bootstrap))
   :configure (resource/phase
               (crates/tomcat))
+  :deploy (resource/phase
+           (crates/tomcat-deploy
+            "../mini-webapp/mini-webapp-1.0.0-SNAPSHOT.war"))
+  :restart-tomcat (resource/phase
+                   (service/service "tomcat6" :action :restart)))
+
+
+(core/defnode haproxy
+  "Simple haproxy"
+  []
+  :bootstrap (resource/phase
+              (crates/bootstrap))
+  :configure (resource/phase
+              (crates/haproxy))
+  :restart-haproxy (resource/phase
+                    (service/service "haproxy" :action :restart)))
+
+(core/defnode proxied
+  "A proxied web app"
+  []
+  :bootstrap (resource/phase
+              (crates/bootstrap))
+  :configure (resource/phase
+              (crates/tomcat)
+              (crates/reverse-proxy :haproxy :app1 8080))
   :deploy (resource/phase
            (crates/tomcat-deploy
             "../mini-webapp/mini-webapp-1.0.0-SNAPSHOT.war"))
