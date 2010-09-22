80f99458229bb6747657814dbd97ec9b2d0aaaab
Hugo Duncan <hugo_duncan@yahoo.com>
2010-09-21 23:48:57 -0400

    switch to jclouds beta-7

--- blank-project/project.clj
+++ blank-project/project.clj
@@ -3,10 +3,9 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [org.cloudhoist/pallet "0.2.0-SNAPSHOT"]
-                 [org.jclouds/jclouds-all "1.0-SNAPSHOT"]
-                 [org.jclouds/jclouds-jsch "1.0-SNAPSHOT"]
-                 [org.jclouds/jclouds-log4j "1.0-SNAPSHOT"]
-                 [org.jclouds/jclouds-enterprise "1.0-SNAPSHOT"]
+                 [org.jclouds/jclouds-all "1.0-beta-7"]
+                 [org.jclouds/jclouds-jsch "1.0-beta-7"]
+                 [org.jclouds/jclouds-log4j "1.0-beta-7"]
                  [log4j/log4j "1.2.14"]]
 
   :dev-dependencies [;;[swank-clojure/swank-clojure "1.2.1"]
