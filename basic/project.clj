a1f43ff534b9e21f60bb015ea3bc3474a5b88367
Antoni Batchelli <tbatchelli@acm.org>
2011-08-20 21:27:12 -0700

    Update swank version in basic project.

--- basic/project.clj
+++ basic/project.clj
@@ -6,7 +6,7 @@
                  [org.jclouds.driver/jclouds-jsch "1.0.0"]
                  [org.jclouds.driver/jclouds-log4j "1.0.0"]
                  [log4j/log4j "1.2.14"]]
-  :dev-dependencies [[swank-clojure/swank-clojure "1.2.1"] ; swank
+  :dev-dependencies [[swank-clojure/swank-clojure "1.3.2"] ; swank
                      [vmfest "0.2.2"]                      ; virtualbox
                      [org.cloudhoist/pallet-lein "0.4.1"]  ; lein
                      [cake-pallet "0.4.0"]]                ; cake
